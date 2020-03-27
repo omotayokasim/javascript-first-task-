a8ff35f9514cff6dfb817ba887140301757eafe9
omotayokasim <smartfocusdrive@gmail.com>
2020-03-27 12:50:15 -0600

    added the index file that links the javascript code

---
+++ maincode.js
@@ -0,0 +1,35 @@
+<script>
+
+let name = 'KASIM OMOTAYO SHARAFADEEN';
+let courses = ['HTML', 'CSS', 'JAVASCRIPT', 'PHP'];
+
+console.log(`My Full name is ${name}`);
+console.log(`The courses that I offer for the start.ng internship program are: 
+${courses}`);
+
+ 
+if (courses.length % 2 == 0)
+{console.log ('The number of courses that I offer is an even number and the following is a list of the even numbers between 1 and 200 inclusive:');
+}
+
+ 
+for (let i = 1; i<=200; i++)
+
+{
+
+if (i % 2 == 0  )
+
+
+{
+console.log(i);
+
+
+}
+
+}
+
+
+
+
+
+</script>
